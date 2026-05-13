7531e8ed6022550451b17000af2d585b0caddf69
ryanlui1222 <64682301+ryanlui1222@users.noreply.github.com>
2026-05-14 04:56:42 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -324,7 +324,6 @@ def main():
         ("https://www.421.news/zh/rss", "421 News", 15, False),
         ("https://www.linking.vision/feed/", "聯經思想空間", 15, False),
         ("https://feedx.net/rss/shanghaishuping.xml", "上海書評", 15, False),
-        ("http://www.eurozine.com/rss.html", "Eurozine", 15, False),
         ("https://www.leapleapleap.com/feed/", "藝術界", 15, False)
     ]
 
