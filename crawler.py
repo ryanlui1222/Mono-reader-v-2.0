785014d3ca61b5d1436f3be1a88c9512e138ce4a
ryanlui1222 <64682301+ryanlui1222@users.noreply.github.com>
2026-05-23 03:52:03 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -519,7 +519,9 @@ def main():
         ("https://radii.co/feed", "Radii", 15, True),
         ("https://www.tcj.com/feed/", "The Comics Journal", 15, True), # 深度評論解析
         ("https://fnmnl.tv/feed/", "FNMNL", 15, False),               # 音樂/街頭快訊
-        ("https://today.duke.edu/topics/arts-&-humanities/rss", "Duke Arts & Humanities", 15, False)
+        ("https://today.duke.edu/topics/arts-&-humanities/rss", "Duke Arts & Humanities", 15, False),
+        ("https://asianreviewofbooks.com/feed/", "Asian Review of Books", 15, False)
+        
     ]
     
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
