a3733c93cfe87e180dadf60119ad3af92624919b
ryanlui1222 <64682301+ryanlui1222@users.noreply.github.com>
2026-05-24 23:12:56 +0800

    Update core_utils.py

--- core_utils.py
+++ core_utils.py
@@ -25,7 +25,8 @@ SOURCE_URLS = {
     "Duke Press": "https://dukeupress.wordpress.com/",
     "Asian Review of Books": "https://asianreviewofbooks.com/",
     "The Comics Journal": "https://www.tcj.com/", "FNMNL": "https://fnmnl.tv/",
-    "触乐": "https://www.chuapp.com/tag/index/id/20369.html"
+    "触乐": "https://www.chuapp.com/tag/index/id/20369.html",
+    "MCLC Resource Center": "https://u.osu.edu/mclc/list/blog/"
 }
 FAST_NEWS_SOURCES = ["WIRED.jp", "CINRA", "VERSE", "界面文化", "Radii", "触乐", "FNMNL"]
 
