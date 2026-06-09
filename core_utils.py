6d2e3015943282408dafca19d674098b654f35c3
ryanlui1222 <64682301+ryanlui1222@users.noreply.github.com>
2026-06-09 17:29:49 +0800

    Update core_utils.py

--- core_utils.py
+++ core_utils.py
@@ -28,7 +28,11 @@ SOURCE_URLS = {
     "The Comics Journal": "https://www.tcj.com/", "FNMNL": "https://fnmnl.tv/",
     "触乐": "https://www.chuapp.com/tag/index/id/20369.html",
     "MCLC Resource Center": "https://u.osu.edu/mclc/list/blog/",
-    "结绳志": "https://tyingknots.net/", "Pharmakon@Matters": "https://matters.town/@Pharmakon"
+    "结绳志": "https://tyingknots.net/", "Pharmakon@Matters": "https://matters.town/@Pharmakon",
+    "波士頓書評": "https://bostonreviewofbooks.substack.com/",
+    "Caja Negra": "https://cajanegraeditora.com.ar/",
+    "Split Infinities": "https://splitinfinities.substack.com/",
+    "FRIEZE": "https://www.frieze.com/"
 }
 FAST_NEWS_SOURCES = ["WIRED.jp", "CINRA", "VERSE", "界面文化", "Radii", "触乐", "FNMNL"]
 
